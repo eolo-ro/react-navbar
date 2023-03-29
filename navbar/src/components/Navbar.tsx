27ca60ad0f664fc09e1dca30f7bdf6e2a6c1489c
eolo-ro <benjamincorderog@gmail.com>
2023-03-29 13:36:01 -0300

    halfway there with only css

--- navbar/src/components/Navbar.tsx
+++ navbar/src/components/Navbar.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+
   return (
     <nav className={styles.nav}>
       <Link to="/" className={styles.siteTitle}>
         Title
       </Link>
-      <ul>
-        <Link to="/about" className="About">
-          About
-        </Link>
-        <Link to="/projects" className="Projects">
-          Projects
-        </Link>
-      </ul>
+
+      <div className={styles.navLinks}>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/about" className="About">
+              About
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/projects" className="Projects">
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <a className={styles.toggleButton}>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+        <span className={styles.bar}></span>
+      </a>
     </nav>
   );
 };
